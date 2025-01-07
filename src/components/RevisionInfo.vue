40b609c5dfe95116e807bb4e0b41ebae39731966
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-01-08 00:39:01 +0800

    refactor(修订信息): 将发布时间和修订时间作为修订版本的索引
    换句话说：“更新”意味着小改动，“修订”意味着大改，需要创建新版本以留存备份。

--- src/components/RevisionInfo.vue
+++ src/components/RevisionInfo.vue
@@ -1,30 +1,23 @@
 <script lang="ts" setup>
-import { percentageRound } from "@/utils/math.ts";
+import { limit } from "@/utils/math.ts";
 import type { integer } from "@vue/language-server";
 import { add, differenceInDays, format, parse } from "date-fns";
 
-const props = defineProps<{ created: string, updated?: string, expired?: integer | string, badge?: integer }>()
-const current = Date.now()
-const createAt = parse(props.created, "yyyy-MM-dd HH:mm", new Date())
-const updateAt = props.updated ? parse(props.updated, "yyyy-MM-dd HH:mm", new Date()) : null
-const expireAt =
-    typeof props.expired !== 'number'
-        ? props.expired ?? null
-        : updateAt !== null
-            ? format(add(updateAt, { days: props.expired }), "yyyy-MM")
-            : format(add(createAt, { days: props.expired }), "yyyy-MM")
+const props = defineProps<{
+    created: string,
+    updated?: string,
+    revised?: string,
+    expired?: integer | string,
+    badge?: integer,
+}>()
 
-const freshness =
-    typeof props.expired !== 'number'
-        ? null
-        : updateAt !== null
-            ? 100 - percentageRound(Math.round(differenceInDays(current, updateAt) / props.expired * 100))
-            : 100 - percentageRound(Math.round(differenceInDays(current, createAt) / props.expired * 100))
+function latest(...days: (Date | null)[]): Date {
+    return days.filter(v => v !== null).sort((a, b) => b!.getTime() - a!.getTime())[0]
+}
 
-const slotsName =
-    props.badge == undefined
-        ? Array.of<string>()
-        : Array.from({ length: props.badge }, (_, index) => index + 1).map(v => `badge${v}`)
+function percentageRound(n: number): number {
+    return limit(0, n, 100)
+}
 
 function getFreshnessColor(percentage: number) {
     if (percentage >= 25)
@@ -33,6 +26,25 @@ function getFreshnessColor(percentage: number) {
         return '#e6a23c'
     return '#f56c6c'
 }
+
+const current = Date.now()
+const createAt = parse(props.created, "yyyy-MM-dd HH:mm", new Date())
+const updateAt = props.updated ? parse(props.updated, "yyyy-MM-dd HH:mm", new Date()) : null
+const reviseAt = props.revised ? parse(props.revised, "yyyy-MM-dd HH:mm", new Date()) : null
+const expireAt = latest(updateAt, reviseAt, createAt)
+const expires =
+    typeof props.expired !== 'number'
+        ? props.expired ?? null
+        : format(add(expireAt, { days: props.expired }), "yyyy-MM")
+const freshness =
+    typeof props.expired !== 'number'
+        ? null
+        : 100 - percentageRound(Math.round(differenceInDays(current, expireAt) / props.expired * 100))
+
+const slotsName =
+    props.badge == undefined
+        ? Array.of<string>()
+        : Array.from({ length: props.badge }, (_, index) => index + 1).map(v => `badge${v}`)
 </script>
 
 <template>
@@ -59,29 +71,29 @@ function getFreshnessColor(percentage: number) {
             </td>
             <td rowspan="4" style="border-left: 1px solid var(--vp-c-divider)"></td>
             <td style="text-wrap: nowrap">
-                <el-text type="info">发布于</el-text>
+                <el-text type="info">{{ props.revised == undefined ? '发布于' : '修订于' }}</el-text>
             </td>
             <td style="text-wrap: nowrap">
-                <el-text type="info">{{ props.created }}</el-text>
+                <el-text type="info">{{ props.revised == undefined ? props.created : props.revised }}</el-text>
             </td>
         </tr>
         <tr v-if="props.updated != undefined">
             <td style="text-wrap: nowrap">
-                <el-text type="info">修订于</el-text>
+                <el-text type="info">更新于</el-text>
             </td>
             <td style="text-wrap: nowrap">
                 <el-text type="info">{{ props.updated }}</el-text>
             </td>
         </tr>
-        <tr v-if="expireAt != null">
+        <tr v-if="expires != null">
             <td style="text-wrap: nowrap">
                 <el-text type="info">保质期</el-text>
             </td>
             <td style="text-wrap: nowrap">
-                <el-text type="info">{{ expireAt }}</el-text>
+                <el-text type="info">{{ expires }}</el-text>
             </td>
         </tr>
-        <tr v-if="freshness !== null">
+        <tr v-if="freshness != null">
             <td style="text-wrap: nowrap">
                 <el-text type="info">新鲜度</el-text>
             </td>
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -5,14 +5,17 @@ export function pi(arr: bigint[]): bigint {
     return arr.reduce((prev, curr) => prev * curr, BigInt(1))
 }
 
+/**
+ * 将数值约束在一个范围内。
+ *
+ * @param floor   最小值。
+ * @param n       被约束的值。
+ * @param ceiling 最大值。
+ */
 export function limit(floor: number, n: number, ceiling: number): number {
     if (floor > n)
         return floor
     if (n > ceiling)
         return ceiling
     return n
-}
-
-export function percentageRound(n: number): number {
-    return limit(0, n, 100)
 }
