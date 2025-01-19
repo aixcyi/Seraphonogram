387b6bc7d3e177392fbb7ad518f342053423fa2e
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-01-20 02:49:12 +0800

    refactor(目录): 为 Element Plus 组件适配 VitePress 色系

--- src/CatalogLabeler.vue
+++ src/CatalogLabeler.vue
@@ -4,6 +4,43 @@ import { data } from "../.vitepress/theme/posts.data.ts";
 
 
 /**
+ * 标签的预设样式。
+ */
+const styles = new Map<string, { type: string }>([
+    // 大类
+    [ '开发', { type: 'danger' } ],
+    [ '测试', { type: 'danger' } ],
+    [ '运维', { type: 'danger' } ],
+    [ '算法', { type: 'danger' } ],
+    [ '设计', { type: 'danger' } ],
+
+    // 技术栈
+    [ 'Python', { type: 'warning' } ],
+    [ 'Kotlin', { type: 'warning' } ],
+    [ 'Golang', { type: 'warning' } ],
+    [ 'Java', { type: 'warning' } ],
+    [ 'IntelliJ 插件', { type: 'warning' } ],
+    [ 'datetime', { type: 'warning' } ],
+    [ 'Django', { type: 'warning' } ],
+    [ 'Django REST Framework', { type: 'warning' } ],
+    [ 'Django OAuth Toolkit', { type: 'warning' } ],
+
+    // 系统、工具、脚手架、依赖包
+    [ 'Windows', { type: 'primary' } ],
+    [ 'Ubuntu', { type: 'primary' } ],
+    [ 'CentOS', { type: 'primary' } ],
+    [ 'PyPI', { type: 'primary' } ],
+    [ 'npm', { type: 'primary' } ],
+
+    // 其它特定主题
+    [ 'IntelliJ IDE', { type: 'success' } ],
+    [ 'LeetCode', { type: 'success' } ],
+    [ '蓝桥杯', { type: 'success' } ],
+    [ 'NOIP', { type: 'success' } ],
+    [ '微信', { type: 'success' } ],
+])
+
+/**
  * 标签开关。
  */
 const switches = defineModel<Map<string, boolean>>('switches', { required: true })
@@ -33,36 +70,6 @@ onMounted(() => {
         _switches.set(_tag, queryTags.has(_tag))
     }
 })
-
-/**
- * 标签的预设样式。
- */
-const styles = new Map<string, { type: string }>([
-    // 大类
-    [ '开发', { type: 'danger' } ],
-    [ '测试', { type: 'danger' } ],
-    [ '运维', { type: 'danger' } ],
-    [ '算法', { type: 'danger' } ],
-    [ '设计', { type: 'danger' } ],
-
-    // 技术栈
-    [ 'Python', { type: 'warning' } ],
-    [ 'Kotlin', { type: 'warning' } ],
-    [ 'Golang', { type: 'warning' } ],
-    [ 'Java', { type: 'warning' } ],
-    [ 'IntelliJ 插件', { type: 'warning' } ],
-    [ 'datetime', { type: 'warning' } ],
-    [ 'Django', { type: 'warning' } ],
-    [ 'Django REST Framework', { type: 'warning' } ],
-    [ 'Django OAuth Toolkit', { type: 'warning' } ],
-
-    // 系统、工具、脚手架、依赖包
-    [ 'Windows', { type: 'success' } ],
-    [ 'Ubuntu', { type: 'success' } ],
-    [ 'CentOS', { type: 'success' } ],
-    [ 'PyPI', { type: 'success' } ],
-    [ 'npm', { type: 'success' } ],
-])
 </script>
 
 <template>
@@ -71,7 +78,7 @@ const styles = new Map<string, { type: string }>([
                    :key="tag"
                    :plain="switches.get(tag)"
                    :text="!switches.get(tag)"
-                   :type="styles.has(tag) ? styles.get(tag)!.type : 'primary'"
+                   :type="styles.has(tag) ? styles.get(tag)!.type : 'info'"
                    round
                    @click="handleSwitch(tag)">
             {{ tag }}&nbsp;
@@ -80,5 +87,55 @@ const styles = new Map<string, { type: string }>([
     </el-space>
 </template>
 
-<style lang="scss" scoped>
+<!--suppress CssUnusedSymbol -->
+<style scoped>
+.el-button--info.is-link, .el-button--info.is-plain, .el-button--info.is-text {
+    --el-button-text-color: #70808F;
+    --el-button-bg-color: var(--vp-c-gray-soft);
+    --el-button-border-color: var(--vp-c-gray-1);
+    --el-button-hover-text-color: white;
+    --el-button-hover-bg-color: var(--vp-c-gray-1);
+    --el-button-hover-border-color: var(--vp-c-gray-1);
+    --el-button-active-text-color: white;
+}
+
+.el-button--primary.is-link, .el-button--primary.is-plain, .el-button--primary.is-text {
+    --el-button-text-color: #47caff;
+    --el-button-bg-color: #174354;
+    --el-button-border-color: #38A0C9;
+    --el-button-hover-text-color: var(--vp-c-bg);
+    --el-button-hover-bg-color: #47caff;
+    --el-button-hover-border-color: #47caff;
+    --el-button-active-text-color: var(--vp-c-bg);
+}
+
+.el-button--success.is-link, .el-button--success.is-plain, .el-button--success.is-text {
+    --el-button-text-color: var(--vp-c-green-1);
+    --el-button-bg-color: var(--vp-c-green-soft);
+    --el-button-border-color: var(--vp-c-green-3);
+    --el-button-hover-text-color: var(--vp-c-bg);
+    --el-button-hover-bg-color: var(--vp-c-green-1);
+    --el-button-hover-border-color: var(--vp-c-green-1);
+    --el-button-active-text-color: var(--vp-c-bg);
+}
+
+.el-button--warning.is-link, .el-button--warning.is-plain, .el-button--warning.is-text {
+    --el-button-text-color: var(--vp-c-yellow-1);
+    --el-button-bg-color: var(--vp-c-yellow-soft);
+    --el-button-border-color: var(--vp-c-yellow-3);
+    --el-button-hover-text-color: var(--vp-c-bg);
+    --el-button-hover-bg-color: var(--vp-c-yellow-1);
+    --el-button-hover-border-color: var(--vp-c-yellow-1);
+    --el-button-active-text-color: var(--vp-c-bg);
+}
+
+.el-button--danger.is-link, .el-button--danger.is-plain, .el-button--danger.is-text {
+    --el-button-text-color: var(--vp-c-red-1);
+    --el-button-bg-color: var(--vp-c-red-soft);
+    --el-button-border-color: var(--vp-c-red-3);
+    --el-button-hover-text-color: var(--vp-c-bg);
+    --el-button-hover-bg-color: var(--vp-c-red-1);
+    --el-button-hover-border-color: var(--vp-c-red-1);
+    --el-button-active-text-color: var(--vp-c-bg);
+}
 </style>
