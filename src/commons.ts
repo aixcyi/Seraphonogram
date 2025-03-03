61b20eb4c0dbbb59f8922302bbd2a6bd5ce2e2ed
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-03-03 11:12:09 +0800

    refactor(卡片链接): 设置回退图标地址，无可用图标时隐藏alt，加粗标题，淡化备注，窄屏隐藏图标

--- src/commons.ts
+++ src/commons.ts
@@ -2,3 +2,9 @@ import { ElDivider } from "element-plus";
 import { h } from "vue";
 
 export const spacer = h(ElDivider, { direction: 'vertical' })
+
+export const favicons: Record<string, string> = {
+    'https://cheatsheets.zip': 'https://cheatsheets.zip/images/favicon.png?v=1',
+    'https://quickref.cn': 'https://quickref.cn/icons/favicon.svg',
+    'https://vitepress.dev': 'https://vitepress.dev/vitepress-logo-mini.svg',
+}
--- src/components/LinkCard.vue
+++ src/components/LinkCard.vue
@@ -1,78 +1,107 @@
 <script lang="ts" setup>
+import { favicons } from "@/commons.ts";
+
 const { href, text, note, logo } = defineProps<{ href?: string, text?: string, note?: string, logo?: string }>()
-const logoSrc = !logo && href ? (new URL(href).origin + '/favicon.ico') : logo
+const image = new Image()
+if (href) {
+    let cursor = 0
+    const root = new URL(href).origin
+    const paths = [
+        ...(logo ? [ logo ] : []),
+        ...(root in favicons ? [ favicons[root] ] : []),
+        `${root}/favicon.ico`,
+        `${root}/favicon.svg`,
+        `${root}/favicon.png`,
+        `${root}/favicon.jpg`,
+        `${root}/favicon.jpeg`,
+        `${root}/icons/favicon.ico`,
+        `${root}/images/favicon.ico`,
+        `${root}/icon/favicon.ico`,
+        `${root}/image/favicon.ico`,
+    ]
+    image.src = paths[cursor]
+    image.onerror = () => {
+        if (cursor >= paths.length)
+            return
+        image.src = paths[++cursor]
+    }
+}
 </script>
 
 
 <template>
     <div class="LinkCard">
         <a :href="href" target="_blank">
-            <p class="note">
-                {{ text }}
+            <p class="desc">
+                <b class="title">{{ text }}</b>
                 <br>
-                <span v-if="note">{{ note }}</span>
-                <span v-else class="VirtualLink">{{ href }}</span>
+                <span v-if="note" class="note">{{ note }}</span>
+                <span v-else class="link">{{ href }}</span>
             </p>
             <div class="logo">
-                <img :src="logoSrc" alt="logo" height="70px" width="70px"/>
+                <img :src="image.src" alt="logo" height="64px" onerror="this.style.display='none'" width="64px"/>
             </div>
         </a>
     </div>
 </template>
 
 
-<style scoped>
+<style lang="scss" scoped>
 /* https://vitepress.yiov.top/components.html#%E9%93%BE%E6%8E%A5%E5%8D%A1%E7%89%87 */
 
-/* 卡片背景 */
 .LinkCard {
     background-color: var(--vp-c-bg-soft);
     border-radius: 8px;
     padding: 8px 16px 8px 8px;
     transition: color 0.5s, background-color 0.5s;
     margin-top: 15px;
+
+    @media (max-width: 500px) {
+        padding: 8px;
+    }
+
+    &:hover {
+        background-color: var(--vp-c-purple-soft);
+    }
+
+    a {
+        text-decoration: none;
+    }
 }
 
-/* 卡片鼠标悬停 */
-.LinkCard:hover {
-    background-color: var(--vp-c-purple-soft);
-}
-
-/* 链接样式 */
 .LinkCard a {
     display: flex;
     align-items: center;
 }
 
-/* 描述链接文字 */
-.LinkCard .note {
+.LinkCard .desc {
     flex: 1;
     font-weight: 500;
-    font-size: 16px;
-    line-height: 25px;
-    color: var(--vp-c-text-1);
-    margin: 0 0 0 16px;
-    transition: color 0.5s;
-}
-
-/* 描述链接文字2 */
-.LinkCard .note span {
     font-size: 14px;
+    line-height: 25px;
+    margin: 16px;
+    transition: color 0.5s;
+
+    .title {
+        color: var(--vp-c-text-1);
+    }
+
+    .note {
+        color: var(--vp-c-text-2);
+    }
+
+    .link {
+        color: var(--vp-c-brand-1);
+    }
 }
 
-/* logo图片 */
 .LinkCard .logo img {
     width: 64px;
     margin: 16px;
     object-fit: contain;
-}
 
-/* 链接下划线去除 */
-.VPDoc a {
-    text-decoration: none;
-}
-
-.VirtualLink {
-    color: var(--vp-c-brand-1);
+    @media (max-width: 500px) {
+        display: none;
+    }
 }
 </style>
