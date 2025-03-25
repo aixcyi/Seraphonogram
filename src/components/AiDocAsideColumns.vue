de1e2eee55920e13094c33620ac1961e759ef20d
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-03-25 11:03:46 +0800

    refactor: 将前端和插件相关博客归到新栏目「黑洞视界面」

--- src/components/AiDocAsideColumns.vue
+++ src/components/AiDocAsideColumns.vue
@@ -8,19 +8,25 @@ const $frontmatter = useData().frontmatter
     <div v-if="$frontmatter.catalog" class="ai-columns VPDoc">
         <p class="card">
             <span class="title">捕蛇者说</span>
-            <span class="colon">／</span>
+            <span class="colon"> · </span>
             <span>围绕 Python 和 Django 及它们的生态撰写的博客。<s>Python 何尝不是一种蛇呢？</s></span>
         </p>
         <hr/>
         <p class="card">
+            <span class="title">黑洞视界面</span>
+            <span class="colon"> · </span>
+            <span>用户可视的相关技术的博客，并不局限于前端，也包括插件、UI、UX 等。<s>听说黑洞的事件视界外能看到自己的后背，超有意思。</s></span>
+        </p>
+        <hr/>
+        <p class="card">
             <span class="title">技术漫谈</span>
-            <span class="colon">／</span>
-            <span>其它与技术有关的博客。Kotlin、TypeScript、Vue 虽是主力，但目前博客较少，所以归到漫谈。</span>
+            <span class="colon"> · </span>
+            <span>其它与技术有关的博客。一些是因为技术栈不常用，比如 Java 和 Golang，一些是本身博客比较少，比如命令行。</span>
         </p>
         <hr/>
         <p class="card">
             <span class="title">题解札</span>
-            <span class="colon">／</span>
+            <span class="colon"> · </span>
             <span>在各个渠道的 Online Judge 刷题时的解题思路、解题方法，以及相关代码。<s>有点菜，别见怪。</s></span>
         </p>
     </div>
---
+++ src/posts/visual/index.md
@@ -0,0 +1,7 @@
+---
+title: 黑洞视界面
+aside: false
+prev: false
+next: false
+order: 2
+---
