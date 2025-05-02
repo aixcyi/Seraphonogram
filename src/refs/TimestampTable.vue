887aff9a7d5b9cdc0039800308b61b99c4529925
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-05-03 01:04:10 +0800

    page/fix/时间戳对照表: 改用等宽字体

--- src/refs/TimestampTable.vue
+++ src/refs/TimestampTable.vue
@@ -45,15 +45,15 @@ const data = [ ...table.values() ].sort(
     <table>
         <thead>
         <tr>
-            <th>存储上限</th>
-            <th>时间上限</th>
+            <th class="sticky-column">存储上限</th>
+            <th class="sticky-column">时间上限</th>
             <th v-for="() in bases" style="text-align: center"></th>
         </tr>
         </thead>
         <tbody>
         <tr v-for="row in data">
-            <td nowrap="nowrap">{{ row.delta }}</td>
-            <td nowrap="nowrap">{{ row.datetime }}</td>
+            <td class="sticky-column" nowrap="nowrap">{{ row.delta }}</td>
+            <td class="sticky-column" nowrap="nowrap">{{ row.datetime }}</td>
             <td v-for="base in bases" style="text-align: center">
                 <span v-if="row.powers.get(base) !== undefined">
                     {{ base }}<sup>{{ row.powers.get(base) }}</sup>
@@ -65,4 +65,9 @@ const data = [ ...table.values() ].sort(
 </template>
 
 <style scoped>
+.sticky-column {
+    /* TODO: 固定表格前两列 */
+    font-family: 'JetBrains Mono', 'MiSans VF', monospace;
+    text-align: right;
+}
 </style>
